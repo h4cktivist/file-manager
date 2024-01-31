c43846abc7419e351add59bd741fb8a83759a93f
h4cktivist <popovvadim0605@gmail.com>
2024-01-31 12:31:38 +0500

    feat: user greeting and stream initialization add

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import readline from 'readline';
 
+const args = process.argv.slice(2);
+const username = args.find(arg => arg.startsWith('--username=')).split('=')[1];
+console.log(`Welcome to the File Manager, ${username}!`);
+
+const readlineStream = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readlineStream.setPrompt('> ');
+readlineStream.prompt();
